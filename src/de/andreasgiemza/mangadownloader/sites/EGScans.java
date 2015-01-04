6a9cf98d6cb5d7641de8f7460620ad3343338862
Andreas Giemza <andreas@giemza.net>
2015-01-04 02:34:22 +0100

    eggscans added

---
+++ src/de/andreasgiemza/mangadownloader/sites/EGScans.java
@@ -0,0 +1,126 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 Andreas Giemza <andreas@giemza.net>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package de.andreasgiemza.mangadownloader.sites;
+
+import de.andreasgiemza.mangadownloader.data.Chapter;
+import de.andreasgiemza.mangadownloader.data.Image;
+import de.andreasgiemza.mangadownloader.data.Manga;
+import de.andreasgiemza.mangadownloader.helpers.JsoupHelper;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ *
+ * @author Andreas Giemza <andreas@giemza.net>
+ */
+public class EGScans implements Site {
+
+    private final String baseUrl = "http://read.egscans.com/";
+
+    @Override
+    public List<Manga> getMangaList() throws IOException {
+        List<Manga> mangas = new LinkedList<>();
+
+        Document doc = JsoupHelper.getHTMLPage(baseUrl);
+
+        Elements rows = doc.select("select[name=manga]").first().select("option");
+
+        for (Element row : rows) {
+            if (row == rows.first()) {
+                continue;
+            }
+
+            mangas.add(new Manga(baseUrl + row.attr("value"), row.text()));
+        }
+
+        return mangas;
+    }
+
+    @Override
+    public List<Chapter> getChapterList(Manga manga) throws IOException {
+        List<Chapter> chapters = new LinkedList<>();
+
+        Document doc = JsoupHelper.getHTMLPage(manga.getLink());
+
+        Elements rows = doc.select("select[name=chapter]").first().select("option");
+
+        for (Element row : rows) {
+            chapters.add(new Chapter(manga.getLink() + "/" + row.attr("value"), row.text()));
+        }
+
+        return chapters;
+    }
+
+    @Override
+    public List<Image> getChapterImageLinks(Chapter chapter) throws IOException {
+        List<Image> images = new LinkedList<>();
+
+        String referrer = chapter.getLink();
+        Document doc = JsoupHelper.getHTMLPage(referrer);
+
+        Elements jss = doc.select("script[type=text/javascript]");
+
+        Element rightJs = null;
+
+        for (Element js : jss) {
+            if (js.toString().contains("img_url.push")) {
+                rightJs = js;
+                break;
+            }
+        }
+
+        if (rightJs == null) {
+            return images;
+        }
+
+        List<String> data = new LinkedList<>();
+
+        Scanner scanner = new Scanner(rightJs.toString());
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.contains("img_url.push('")) {
+                data.add(line);
+            }
+        }
+
+        if (data.isEmpty()) {
+            return images;
+        }
+
+        for (String line : data) {
+            String link = baseUrl + line.split("img_url\\.push\\(\\'")[1].split("\\' \\);")[0];
+            String extension = link.substring(link.length() - 3, link.length());
+
+            images.add(new Image(link, referrer, extension));
+        }
+
+        return images;
+    }
+}
