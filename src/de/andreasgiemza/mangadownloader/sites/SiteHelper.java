e934d762edbf0101ccf99677cde6544de026bc41
Andreas Giemza <andreas@giemza.net>
2015-01-05 18:52:48 +0100

    removed imports ...

--- src/de/andreasgiemza/mangadownloader/sites/SiteHelper.java
+++ src/de/andreasgiemza/mangadownloader/sites/SiteHelper.java
@@ -27,8 +27,6 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import org.reflections.Reflections;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  *
