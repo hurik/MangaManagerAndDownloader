c5fc716586e63e0ae5e098b814428f5f5eb25293
Andreas Giemza <andreas@giemza.net>
2015-03-17 14:43:55 +0100

    Deactivated MeinManga (Site is down)

--- src/de/andreasgiemza/mangadownloader/sitesdown/MeinManga.java
+++ src/de/andreasgiemza/mangadownloader/sitesdown/MeinManga.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package de.andreasgiemza.mangadownloader.sites.implementations.german;
+package de.andreasgiemza.mangadownloader.sitesdown;
 
 import de.andreasgiemza.mangadownloader.data.Chapter;
 import de.andreasgiemza.mangadownloader.data.Image;
