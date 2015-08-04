20ecdabda3801fe2374fe1109a3c86328453917e
Andreas Giemza <andreas@giemza.net>
2015-08-04 22:11:42 +0200

    Main class was missing

--- src/main/java/de/andreasgiemza/mangadownloader/MangaDownloader.java
+++ src/main/java/de/andreasgiemza/mangadownloader/MangaDownloader.java
@@ -738,6 +738,36 @@ public class MangaDownloader extends javax.swing.JFrame {
         downloadsTableModel.fireTableDataChanged();
     }//GEN-LAST:event_removeDownloadButtonActionPerformed
 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Windows look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Windows is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MangaDownloader.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MangaDownloader().setVisible(true);
+            }
+        });
+    }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JCheckBox chapterDeSelectAllCheckBox;
     private javax.swing.JPanel chapterListPanel;
